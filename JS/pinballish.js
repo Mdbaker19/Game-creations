c84b5ae7518bf959b47e779cd8f1827c72e2b090
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-01 13:28:52 -0500

    added top and bottom end movement for both paddles

--- JS/pinballish.js
+++ JS/pinballish.js
@@ -7,6 +7,13 @@
 
     let canvasContext;
 
+    const leftBumperX = 150;
+    const leftBumperY = 300;
+    const rightBumperX = 350;
+    const rightBumperY = 300;
+    const bumperWidth = 30;
+    const bumperHeight = 30;
+
     const paddleWidth = 175;
     const paddleHeight = 25;
     //leaves a 65px range for ball to fall through
@@ -53,13 +60,17 @@
         controlPaddles();
         paddleRetract();
         restartBall();
+        bumperHit();
     }
 
     function drawEverything(){
-        colorRectangles(0, 0, canvas.width, canvas.height, "lightblue");
+        colorRectangles(0, 0, canvas.width, canvas.height, "lightblue");// for canvas
+        colorRectangles(leftBumperX, leftBumperY, bumperWidth, bumperHeight, "orange");//for left bumper
+        colorRectangles(rightBumperX, rightBumperY, bumperWidth, bumperHeight, "orange");//for right bumper
+        colorRectangles(ballX, ballY, 20, 20, "bisque");// for ball
         colorRectangles(leftPaddleX, leftPaddleY, paddleWidth, paddleHeight, "black");
         colorRectangles(rightPaddleX, rightPaddleY, paddleWidth, paddleHeight, "red");
-        colorRectangles(ballX, ballY, 20, 20, "bisque");
+        //canvas.fillText("score will go here", 250, 100);
     }
 
 
@@ -100,11 +111,23 @@
     }
 
 
+    function bumperHit(){
+        //work on this later, a bit of math to do....
+    }
+
     function paddleRetract(){
         if(rightPaddleX < 325){
             rightPaddleX += 12.5;
         } if(leftPaddleX > 0){
             leftPaddleX -= 12.5;
+        }if(rightPaddleY >= 1000){
+            rightPaddleY -= paddleMove;
+        }if(leftPaddleY >= 1000) {
+            leftPaddleY -= paddleMove;
+        }if(rightPaddleY <= 800){
+            rightPaddleY += paddleMove;
+        }if(leftPaddleY <= 800){
+            leftPaddleY += paddleMove;
         }
     }
 
