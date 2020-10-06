c1613623601638713b9c649da9e00d4f79c5dabd
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-05 22:09:49 -0500

    added restart to each scratch for each goal

--- JS/pool.js
+++ JS/pool.js
@@ -3,8 +3,8 @@
     let player;
     let playerX = 390;
     let playerY = 770;
-    let playerWidth = 20;
-    let playerHeight = 20;
+    let ballWidth = 20;
+    let ballHeight = 20;
     let canvas;
     let canvasContext;
     //canvas is 800 by 800
@@ -35,11 +35,38 @@
     let bottomRightX = 750;
     let bottomRightY = 750;
 
+    let ball1 = {
+        x: 400,
+        y: 200
+    };
+    let ball2 = {
+        x: 380,
+        y: 170
+    };
+    let ball3 = {
+        x: 420,
+        y: 170
+    };
+    let ball4 = {
+        x: 440,
+        y: 140
+    };
+    let ball5 = {
+        x: 400,
+        y: 140
+    };
+    let ball6 = {
+        x: 360,
+        y: 140
+    }
+
+
 
     window.onload = function game(){
         canvas = document.getElementById("gamecanvas");
         canvasContext = canvas.getContext("2d");
-        setInterval(callFunctions, 200);
+        setInterval(callFunctions, 100);
+        setInterval(positionCheck, 500);
 
         window.addEventListener("keydown", function move(evt){
             const direction = evt.key.replace("Arrow", "");
@@ -50,8 +77,11 @@
     function callFunctions(){
         drawEverything();
         movePlayer();
-        scratch_BottomLeftForNow();
-        //console.log(playerY, playerX);
+        scratch();
+    }
+
+    function positionCheck(){
+        console.log(playerX, playerY);
     }
 
     // to make this process shorter too? with collectively coloring in balls with an object or something??
@@ -70,7 +100,13 @@
         colorIn(bottomLeftX, bottomLeftY, goalDimensions, goalDimensions, goal);
         colorIn(bottomRightX, bottomRightY, goalDimensions, goalDimensions, goal);
 
-        colorIn(playerX, playerY, playerWidth, playerHeight, "white");
+        colorIn(ball1.x, ball1.y, ballWidth, ballHeight, "blue");
+        colorIn(ball2.x, ball2.y, ballWidth, ballHeight, "red");
+        colorIn(ball3.x, ball3.y, ballWidth, ballHeight, "green");
+        colorIn(ball4.x, ball4.y, ballWidth, ballHeight, "yellow");
+        colorIn(ball5.x, ball5.y, ballWidth, ballHeight, "black");
+        colorIn(ball6.x, ball6.y, ballWidth, ballHeight, "lightblue");
+        colorIn(playerX, playerY, ballWidth, ballHeight, "white");
 
     }
 
@@ -115,8 +151,29 @@
 
     // higher order functions, objects
 
-    function scratch_BottomLeftForNow (){
-        if(playerX < 50 && playerY > 750){
+    function scratch(){
+        //bottom left
+        if(playerX < bottomLeftX + goalDimensions && playerY >= bottomLeftY){
+            startPosition();
+        }
+        //bottom right
+        if(playerX >= bottomRightX && playerY >= bottomRightY){
+            startPosition();
+        }
+        //middle left
+        if(playerY >= 370 && playerY < middleLeftY + goalDimensions && playerX < goalDimensions){
+            startPosition();
+        }
+        //middle right
+        if(playerY >= 370 && playerY <= middleRightY + goalDimensions && playerX >= middleRightX){
+            startPosition();
+        }
+        //top right
+        if(playerY < goalDimensions && playerX >= canvas.width - goalDimensions){
+            startPosition();
+        }
+        //top left
+        if(playerY < goalDimensions && playerX < goalDimensions){
             startPosition();
         }
     }
@@ -142,7 +199,7 @@
     //         }
     //     }
     // }
-    //
+
     // function showBalls(){
     //     return goal.held_balls
     // }
