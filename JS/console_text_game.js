7d80b69530141797a4df7a01fdf6eaf08846657e
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-06 16:46:05 -0500

    added abilities

--- JS/console_text_game.js
+++ JS/console_text_game.js
@@ -5,32 +5,40 @@
             firstName: "matt",
             lastName: "b",
             age: 25
-        }
+        },
+        abilities: ["fix", "code", "install"]
     },
         {
         person: {
             firstName: "mandy",
             lastName: "b",
             age: 25
-        }
+        },
+        abilities: ["cook", "design", "care"]
     },
         {
         person: {
             firstName: "nemo",
             lastName: "goat Snake",
             age: 2
-        }
+        },
+        abilities: ["be weird", "groan", "paw"]
     },
         {
         person: {
             firstName: "sabear",
             lastName: "say say",
             age: 3
-        }
-        }]
+        },
+        abilities: ["guard", "drink", "listen"]
+    }]
 
 players.forEach(function(player){
     console.log(player.person.firstName);
+    player.abilities.forEach(function(ability){
+        console.log(ability);
+    });
+    console.log("----SPACE-------")
 });
 
 
