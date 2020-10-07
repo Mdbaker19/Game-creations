bdf6cf63dc1eb637cabdfffd4bf81c910da9f0a7
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-07 08:54:19 -0500

    added names above each drawing for each player

--- JS/console_text_game.js
+++ JS/console_text_game.js
@@ -3,6 +3,8 @@
     let canvas;
     let ctx;
     let dimensions = 35;
+    let nameSpot = dimensions/7;
+    let nameHeight = 10;
 
     let players = [{
         matt: {
@@ -45,7 +47,9 @@
         abilities: ["guard", "drink", "listen"]
     }]
 
-    console.log(players.matt.x);
+
+
+    //console.log(players[0].matt.x)
 
 // players.forEach(function(player){
 //     console.log(player.person.firstName);
@@ -67,15 +71,19 @@
     }
 
     function drawEverything(){
-        colorIn(0, 0, canvas.width, canvas.height, "#1eaea1")
-        colorIn(players[0].x, players[0].y, dimensions, dimensions, "black");
-        //ctx.fillText();
-        colorIn();
-        //ctx.fillText();
-        colorIn();
-        //ctx.fillText();
-        colorIn();
-        //ctx.fillText();
+        colorIn(0, 0, canvas.width, canvas.height, "#1eaea1");
+
+        colorIn(players[0].matt.x, players[0].matt.y, dimensions, dimensions, "#081b83");
+        ctx.fillText(players[0].matt.firstName, players[0].matt.x + nameSpot, players[0].matt.y - nameHeight);
+
+        colorIn(players[1].mandy.x, players[1].mandy.y, dimensions, dimensions, "#95f469");
+        ctx.fillText(players[1].mandy.firstName, players[1].mandy.x + nameSpot, players[1].mandy.y - nameHeight);
+
+        colorIn(players[2].nemo.x, players[2].nemo.y, dimensions, dimensions, "black");
+        ctx.fillText(players[2].nemo.firstName, players[2].nemo.x + nameSpot, players[2].nemo.y - nameHeight);
+
+        colorIn(players[3].sabear.x, players[3].sabear.y, dimensions, dimensions, "#cbb64f");
+        ctx.fillText(players[3].sabear.firstName, players[3].sabear.x + nameSpot, players[3].sabear.y - nameHeight);
     }
 
     function colorIn(leftX, topY, width, height, color){
