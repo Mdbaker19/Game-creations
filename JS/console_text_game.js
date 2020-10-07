04d738f94d1687a197f49c8c5284ebb63a1b31f5
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-06 19:07:22 -0500

    stuck on the object part

--- JS/console_text_game.js
+++ JS/console_text_game.js
@@ -1,7 +1,13 @@
 (function (){
 
-    const players = [{
-        person: {
+    let canvas;
+    let ctx;
+    let dimensions = 35;
+
+    let players = [{
+        matt: {
+            x: 100,
+            y: 100,
             firstName: "matt",
             lastName: "b",
             age: 25
@@ -9,7 +15,9 @@
         abilities: ["fix", "code", "install"]
     },
         {
-        person: {
+        mandy: {
+            x: 200,
+            y: 200,
             firstName: "mandy",
             lastName: "b",
             age: 25
@@ -17,7 +25,9 @@
         abilities: ["cook", "design", "care"]
     },
         {
-        person: {
+        nemo: {
+            x: 300,
+            y: 300,
             firstName: "nemo",
             lastName: "goat Snake",
             age: 2
@@ -25,7 +35,9 @@
         abilities: ["be weird", "groan", "paw"]
     },
         {
-        person: {
+        sabear: {
+            x: 400,
+            y: 400,
             firstName: "sabear",
             lastName: "say say",
             age: 3
@@ -33,13 +45,42 @@
         abilities: ["guard", "drink", "listen"]
     }]
 
-players.forEach(function(player){
-    console.log(player.person.firstName);
-    player.abilities.forEach(function(ability){
-        console.log(ability);
-    });
-    console.log("----SPACE-------")
-});
+    console.log(players.matt.x);
 
+// players.forEach(function(player){
+//     console.log(player.person.firstName);
+//     player.abilities.forEach(function(ability){
+//         console.log(ability);
+//     });
+//     console.log("----SPACE-------")
+// });
+
+
+    window.onload = function startGame(){
+        canvas = document.getElementById("gamecanvas");
+        ctx = canvas.getContext("2d");
+        setInterval(callFunctions, 100);
+    }
+
+    function callFunctions(){
+        drawEverything();
+    }
+
+    function drawEverything(){
+        colorIn(0, 0, canvas.width, canvas.height, "#1eaea1")
+        colorIn(players[0].x, players[0].y, dimensions, dimensions, "black");
+        //ctx.fillText();
+        colorIn();
+        //ctx.fillText();
+        colorIn();
+        //ctx.fillText();
+        colorIn();
+        //ctx.fillText();
+    }
+
+    function colorIn(leftX, topY, width, height, color){
+        ctx.fillStyle = color;
+        ctx.fillRect(leftX, topY, width, height);
+    }
 
 })();
