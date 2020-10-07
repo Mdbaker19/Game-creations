2a172d80da0d33ea65819bccdd98d24e79f46617
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-07 18:37:09 -0500

    cleaned up objects a little

--- JS/object_characters.js
+++ JS/object_characters.js
@@ -2,45 +2,45 @@
 
     let canvas;
     let ctx;
-    let dimensions = 35;
-    let nameSpot = dimensions/7;
+    let dimensions = 65;
+    let nameSpot = dimensions/5;
     let nameHeight = 10;
 
     let players = [{
-        matt: {
+        person: {
             x: 100,
             y: 100,
-            firstName: "matt",
+            firstName: "Matt",
             lastName: "b",
             age: 25
         },
         abilities: ["fix", "code", "install"]
     },
         {
-        mandy: {
+        person: {
             x: 200,
             y: 200,
-            firstName: "mandy",
+            firstName: "Mandy",
             lastName: "b",
             age: 25
         },
         abilities: ["cook", "design", "care"]
     },
         {
-        nemo: {
+        person: {
             x: 300,
             y: 300,
-            firstName: "nemo",
+            firstName: "Nemo",
             lastName: "goat Snake",
             age: 2
         },
         abilities: ["be weird", "groan", "paw"]
     },
         {
-        sabear: {
+        person: {
             x: 400,
             y: 400,
-            firstName: "sabear",
+            firstName: "Sabear",
             lastName: "say say",
             age: 3
         },
@@ -51,13 +51,13 @@
 
     //console.log(players[0].matt.x)
 
-// players.forEach(function(player){
-//     console.log(player.person.firstName);
-//     player.abilities.forEach(function(ability){
-//         console.log(ability);
-//     });
-//     console.log("----SPACE-------")
-// });
+players.forEach(function(player){
+    console.log(player.person.firstName);
+    player.abilities.forEach(function(ability){
+        console.log(ability);
+    });
+    console.log("----SPACE-------")
+});
 
 
     window.onload = function startGame(){
@@ -73,17 +73,17 @@
     function drawEverything(){
         colorIn(0, 0, canvas.width, canvas.height, "#1eaea1");
 
-        colorIn(players[0].matt.x, players[0].matt.y, dimensions, dimensions, "#081b83");
-        ctx.fillText(players[0].matt.firstName, players[0].matt.x + nameSpot, players[0].matt.y - nameHeight);
+        colorIn(players[0].person.x, players[0].person.y, dimensions, dimensions, "#081b83");
+        ctx.fillText(players[0].person.firstName, players[0].person.x + nameSpot, players[0].person.y - nameHeight);
 
-        colorIn(players[1].mandy.x, players[1].mandy.y, dimensions, dimensions, "#95f469");
-        ctx.fillText(players[1].mandy.firstName, players[1].mandy.x + nameSpot, players[1].mandy.y - nameHeight);
+        colorIn(players[1].person.x, players[1].person.y, dimensions, dimensions, "#95f469");
+        ctx.fillText(players[1].person.firstName, players[1].person.x + nameSpot, players[1].person.y - nameHeight);
 
-        colorIn(players[2].nemo.x, players[2].nemo.y, dimensions, dimensions, "black");
-        ctx.fillText(players[2].nemo.firstName, players[2].nemo.x + nameSpot, players[2].nemo.y - nameHeight);
+        colorIn(players[2].person.x, players[2].person.y, dimensions, dimensions, "black");
+        ctx.fillText(players[2].person.firstName, players[2].person.x + nameSpot, players[2].person.y - nameHeight);
 
-        colorIn(players[3].sabear.x, players[3].sabear.y, dimensions, dimensions, "#cbb64f");
-        ctx.fillText(players[3].sabear.firstName, players[3].sabear.x + nameSpot, players[3].sabear.y - nameHeight);
+        colorIn(players[3].person.x, players[3].person.y, dimensions, dimensions, "#cbb64f");
+        ctx.fillText(players[3].person.firstName, players[3].person.x + nameSpot, players[3].person.y - nameHeight);
     }
 
     function colorIn(leftX, topY, width, height, color){
