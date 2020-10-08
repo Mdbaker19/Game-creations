030753b34e1e00709e4851b85ee9bf312b1fbde9
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-08 10:50:49 -0500

    added a score for the alien as well

--- JS/shootingGame.js
+++ JS/shootingGame.js
@@ -1,15 +1,17 @@
 (function (){
     let cvs;
     let ctx;
-    let score = 0;
     const dimensions = 50;
     const alien = {
         x: 1000,
-        y: 0
+        y: 0,
+        speed: 20,
+        score: 0
     }
     const player = {
         x: 100,
         y: 450,
+        score: 0,
         shoot: function fire(){
             fill(this.x + dimensions/4, 0, dimensions/2, cvs.height-dimensions * 2, "#a44f19");
         }
@@ -21,11 +23,11 @@
         setInterval(load, 50);
 
         window.addEventListener("mousedown", function fire(){
-            console.log("clicked");
+            console.log("FIRE!");
             player.shoot();
             if(alien.x > player.x - dimensions/2 && alien.x < player.x +dimensions/2){
                 alien.x = cvs.width;
-                score++
+                player.score++
             }
         })
     }
@@ -39,7 +41,8 @@
         fill(0,0, cvs.width, cvs.height, "#4c1070");//canvas
         fill(alien.x, alien.y, dimensions, dimensions, "black");//alien
         fill(player.x, player.y, dimensions, dimensions, "#66dbca"); //player
-        ctx.fillText(score, 500, 200);//score
+        ctx.fillText("successful hits: " + player.score, 500, 200);//score
+        ctx.fillText("aliens that survived: " + alien.score, 500, 210);//alien score
     }
 
     function fill(leftX, topY, width, height, color){
@@ -50,8 +53,9 @@
     function moveAlien(){
         if(alien.x <= -50){
             alien.x = cvs.width;
+            alien.score++;
         }
-        alien.x -= 10;
+        alien.x -= alien.speed;
     }
 
 })();
